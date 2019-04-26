b168dc1705ba829670f8af4f0d0698d9cd563c3c
Jean <jean@yolo.com>
2019-04-26 14:23:35 +0200

    Cleaner code with new topic for pump and window + topic recognition

---
+++ Code/Arduino/GreenFarm/GreenFarm.ino
@@ -0,0 +1,165 @@
+//#include <ArduinoNATS.h>
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+
+// Function prototypes
+void subscribeReceive(char* topic, byte* payload, unsigned int length);
+ 
+// Set your MAC address and IP address here
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+// IP du shield run DHCPAdressPrinter
+IPAddress ip(192, 168, 1, 21);
+//IPAddress ip(Ethernet.localIP());
+ 
+// Make sure to leave out the http and slashes!
+//const char* server = "test.mosquitto.org";
+const char *serverHostname = "raspberrypi";
+const IPAddress serverIPAddress(192, 168, 1, 4);
+
+EthernetClient ethClient;
+PubSubClient mqttClient(ethClient);
+
+
+// Mqtt topics definition
+const char *pub_temp = "GreenFarm/Arduino/Temperature";
+const char *pub_hum = "GreenFarm/Arduino/Humidity";
+const char *pub_moist = "GreenFarm/Arduino/Moist";
+
+char *sub_window = "GreenFarm/Raspberry/Window";
+char *sub_pump = "GreenFarm/Raspberry/Pump";
+
+
+
+
+void setup(){
+
+    // Useful for debugging purposes
+  Serial.begin(9600);
+
+
+/**
+ *  Ethernet and Mqtt server
+ *  
+ *  Server run on raspberry pi : hostname raspberrypi and ip = 193.168.1.4
+ *  
+ *  
+ */
+  
+  // Start the ethernet connection
+  Ethernet.begin(mac, ip);
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }        
+  Serial.println("Connected to shield") ;  
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+  Serial.println("Cable connected") ;  
+  // Ethernet takes some time to boot!
+  delay(3000);                          
+
+  // Set the MQTT server to the server stated above ^
+  mqttClient.setServer(serverIPAddress, 1883);
+  Serial.println("Connecting ...");
+  while (!mqttClient.connect("myClientID")) {
+    delay(1000);
+  }
+  Serial.println("Connected ! ");
+    // Establish the subscribe event
+  mqttClient.setCallback(subscribeReceive);
+
+/**
+ *  Green Farm configuration
+ *  
+ */
+  
+  
+
+  
+}
+
+void loop(){
+ // This is needed at the top of the loop!
+  mqttClient.loop();
+ 
+  // Subscribe to Raspberry Pump topic
+  mqttClient.subscribe(sub_pump);
+  mqttClient.subscribe(sub_window);
+
+ 
+  // Publish Temperature on Server"
+  int temp = 19;
+  if(mqttClient.publish(pub_temp, temp))
+  {
+    Serial.println("published message");
+  }
+  else
+  {
+    Serial.println("Could not send message :(");
+  }
+
+  
+ 
+  // Dont overload the server!
+  delay(4000);
+    
+
+
+  
+}
+
+void subscribeReceive(char* topic, byte* payload, unsigned int length)
+{
+  // Print the topic
+  Serial.print("Topic: ");
+  Serial.println(topic);
+ 
+  // Print the message
+  Serial.print("Received Message: ");
+  char *data = "";
+  for(int i = 0; i < length; i ++)
+  {
+    Serial.print(char(payload[i]));
+    data += char(payload[i]);
+  }
+  Serial.println("");
+
+  Serial.println(sub_pump);
+  if (strcmp (topic,"GreenFarm/Raspberry/Pump") == 0){
+    Serial.println("Received Pump order");
+    }
+  else if (strcmp (topic,"GreenFarm/Raspberry/Window") == 0){
+    Serial.println("Received  Window order");
+    }
+  else{
+      Serial.println("nothing");
+    }
+
+/*
+  switch (char(topic)){
+    
+    case  char(sub_pump:
+
+      break;
+
+    case sub_window:
+
+      break;
+    }
+
+*/
+
+  
+
+  
+  
+ 
+  // Print a newline
+  Serial.println("");
+}
